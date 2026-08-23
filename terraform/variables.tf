b62d524914c7167588b4e72a487f59a770e95ea2
iam-dushyant <dushyant.khatri@icloud.com>
2026-08-23 22:01:11 +0100

    Created Terraform config files

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "region" {
+    description = "The GCP region"
+    type        = string
+}
+
+variable "artifact_repository" {
+    description = "The GCP Artifact Registry name"
+    type        = string
+    default = "ml-platform-v2"
+}
+
+variable "cluster_name" {
+    description = "The GKE Autopilot cluster name"
+    type        = string
+}
+
+variable "model_bucket_name" {
+    description = "The GCP bucket name for storing ML models"
+    type        = string
+}
